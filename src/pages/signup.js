aa1230b43f183921d4dd09083dd9ad033dd51b15
Prénom Nom <monemail@fournisseur.com>
2024-07-02 14:25:36 +0200

    change signup

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -69,7 +69,7 @@ export default function Signup() {
     }
 
     try {
-      const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}auth/signup`, {
+      const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/signup`, {
         firstname: formData.firstname,
         lastname: formData.lastname,
         username: formData.username,
